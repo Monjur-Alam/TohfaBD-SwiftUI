7721914739e4c8be9f44409e0d0445ad74cf8c54
macbookpro <munjur533@gmail.com>
2022-12-28 17:15:21 +0600

    Initial Commit

---
+++ Tohfabd/TohfabdApp.swift
@@ -0,0 +1,17 @@
+//
+//  TohfabdApp.swift
+//  Tohfabd
+//
+//  Created by MacBook Pro on 28/12/22.
+//
+
+import SwiftUI
+
+@main
+struct TohfabdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
